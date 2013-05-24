9920f549baffe315ab7a5965111e2d25ad0716e5
Brian Lachniet <blachniet@gmail.com>
2013-05-24 09:03:03 -0400

    Updated powershell command to an application level command. Added name keys to commands.

--- blachniet.py
+++ blachniet.py
@@ -5,20 +5,36 @@ import sublime_plugin
 
 class BlachnietTextCommand(sublime_plugin.TextCommand):
     def get_working_dir(self):
-        '''Gets the current working directory of the current document. Returns None
+        '''Gets the current working directory of the current view. Returns None
         if the current document has not been saved.
-
+        
         '''
-        file_name = self.view.file_name()
-        if file_name is None:
+        if self.view is None or self.view.file_name() is None:
             return None
         return os.path.dirname(self.view.file_name())    
+
+class BlachnietApplicationCommand(sublime_plugin.ApplicationCommand):
+    def get_working_dir(self):
+        '''Gets the current working directory of the active view in the active window.
+        If the active window is None, active view is None, or the view has not been saved,
+        then None will be returned
+        
+        '''
+        active_window = sublime.active_window()
+        if active_window is not None:
+            active_view = active_window.active_view()
+            if active_view is not None:
+                file_name = active_view.file_name()
+                if file_name is not None:
+                    return os.path.dirname(file_name)
+        return None
 
 class OpenContainingFolder(BlachnietTextCommand):
     '''Opens a file browser to the current file's containing folder. If the current file has
     not been saved, then the file browser will be opened to some default location.
 
     '''
+
     def run(self, edit):
         if os.name == 'nt':
             sublime.status_message("Opening folder...")
@@ -27,18 +43,20 @@ class OpenContainingFolder(BlachnietTextCommand):
             if working_dir is None:
                 subprocess.Popen('explorer.exe')
             else:
-                subprocess.Popen('explorer.exe "' + self.get_working_dir() + '"')
+                subprocess.Popen('explorer.exe "' + working_dir + '"')
         else:
             sublime.status_message("Could not open folder. This feature is only supported on Windows.")
 
-class OpenPowerShellHere(BlachnietTextCommand):
+class OpenPowerShellHere(BlachnietApplicationCommand):
     '''Opens powershell.exe to the current file's directory. If the current file has not been saved, this command will 
     open powershell to the user's directory (~)
-    
+
     '''
-    def run(self, edit):
+
+    def run(self):
         if os.name == 'nt':
-            sublime.status_message("Opening PowerShell");
+            sublime.status_message("Opening PowerShell...");
+
             working_dir = self.get_working_dir()
             if working_dir is None:
                 subprocess.Popen('powershell.exe -noexit -command "cd \'~\'"')
